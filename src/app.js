d3359fddef552d05c59a75dc78bbd3d6397c3aa0
Charles <charleslanop@gmail.com>
2021-09-16 22:43:35 -0300

    veljs update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,27 @@
-const vel = new Vel();
-vel.callback = Vel.typeCallback.backgroundColor;
-vel.listObject = ['gray'];
-vel.setListClass(['test_class1']);
+const vel = new Vel(Vel.feedbackLanguage.pt);
+
+const linkA = async () => {
+  vel.url =
+    'https://raw.githubusercontent.com/charleslana/naruto-legacy-client/master/pages/';
+  vel.object = 'main';
+  await vel.routePage('home');
+  vel.callback = Vel.typeCallback.text;
+  const result = vel.getId('pageHome');
+  console.log(result);
+  const hash = window.location.hash.substring(2);
+  console.log(hash);
+};
+
+const linkB = async () => {
+  vel.url = 'https://viacep.com.br/ws/01001000/json/';
+  await vel.fetchData(Vel.methodFetchData.get);
+  const result = vel.data;
+  console.log(result);
+  vel.object = result['localidade'];
+  vel.callback = Vel.typeCallback.text;
+  vel.setId('main');
+};
+
+vel.callback = Vel.typeCallback.click;
+vel.listObject = [linkA, linkB];
+vel.setListId(['link_a', 'link_b']);
--- src/vel.js
+++ src/vel.js
@@ -2,19 +2,94 @@
 VelJs is Velocity JavaScript by CharlesLana
 A library that automates some features and returns feedback to the user
 */
+
 class Vel {
-  object = '';
+  constructor(language = Vel.feedbackLanguage.en) {
+    this.#feedbackLanguage = language;
+  }
+
+  #feedbackLanguage;
+  object;
   listObject = [];
   callback;
-  #mask = /[^A-zÀ-ú0-9_\s]/gi;
+  #maskRemoveSpecialCharacters = /[^A-zÀ-ú0-9_\s]/gi;
+  url = '';
+  extensionPath = 'html';
+  loading = 'Loading...';
+  isHash = true;
+  data;
+
+  static feedbackLanguage = {
+    en: 'en',
+    pt: 'pt',
+  };
 
   static typeCallback = {
     text: 'text',
     image: 'image',
-    color: 'color',
-    display: 'display',
-    backgroundColor: 'backgroundColor',
+    style: {
+      color: 'color',
+      display: 'display',
+      backgroundColor: 'backgroundColor',
+      zIndex: 'zIndex',
+      backgroundImage: 'backgroundImage',
+      fontSize: 'fontSize',
+      fontWeight: 'fontWeight',
+      height: 'height',
+      width: 'width',
+      visibility: 'visibility',
+      opacity: 'opacity',
+    },
+    click: 'click',
   };
+
+  static methodFetchData = {
+    get: 'GET',
+    post: 'POST',
+    put: 'PUT',
+  };
+
+  async fetchData(method) {
+    this.#checkObjectNotNull(method);
+    await fetch(this.url, { method: method })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(this.#exceptionErrorMessage('failedAPI'));
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.data = data;
+      })
+      .catch((error) => {
+        this.#messageError(error.message);
+      });
+  }
+
+  async routePage(path) {
+    this.#checkObjectNotNull(path);
+    this.#checkObjectNotFound();
+    const element = document.querySelectorAll(`[id="${this.object}"]`);
+    this.#checkMultipleId(element);
+    element[0].innerHTML = this.loading;
+    await fetch(`${this.url}${path}.${this.extensionPath}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(this.#exceptionErrorMessage('failedAPI'));
+        }
+        return response.text();
+      })
+      .then((data) => {
+        element[0].innerHTML = data;
+        if (this.isHash) {
+          window.location.hash = `#/${path}`;
+        }
+      })
+      .catch((error) => {
+        element[0].innerHTML = error.message;
+        this.#messageError(error.message);
+      });
+  }
 
   getId(object) {
     this.#checkObjectNotNull(object);
@@ -24,38 +99,63 @@ class Vel {
   }
 
   setId(object) {
-    object = object.trim();
     this.#checkObjectNotNull(object);
+    this.#checkObjectNotFound();
     const element = document.querySelectorAll(`[id="${object}"]`);
     this.#checkMultipleId(element);
-    this.#checkTextNotFound();
     this.#setCallback(element[0], this.object);
+  }
+
+  getListId(listObject) {
+    this.#checkIsArray(listObject);
+    const newListObject = [];
+    listObject.forEach((value) => {
+      this.#checkObjectNotNull(value);
+      let element = document.querySelectorAll(`[id="${value}"]`);
+      this.#checkMultipleId(element);
+      element.forEach((value) => {
+        newListObject.push(this.#getCallback(value));
+      });
+    });
+    return newListObject;
+  }
+
+  setListId(listObject) {
+    this.#checkIsArray(listObject);
+    this.#checkObjectListNotFound(listObject);
+    listObject.forEach((value, indexListObject) => {
+      this.#checkObjectNotNull(value);
+      let element = document.querySelectorAll(`[id="${value}"]`);
+      this.#checkMultipleId(element);
+      element.forEach((value, index) => {
+        this.#setCallback(element[index], this.listObject[indexListObject]);
+      });
+    });
   }
 
   getOnlyClass(object) {
     this.#checkObjectNotNull(object);
     const element = document.querySelectorAll(
-      `.${object.replace(this.#mask, '')}`
+      `.${object.replace(this.#maskRemoveSpecialCharacters, '')}`
     );
     this.#checkMultipleClass(element);
     return this.#getCallback(element[0]);
   }
 
   setOnlyClass(object) {
-    object = object.trim();
     this.#checkObjectNotNull(object);
+    this.#checkObjectNotFound();
     const element = document.querySelectorAll(
-      `.${object.replace(this.#mask, '')}`
+      `.${object.replace(this.#maskRemoveSpecialCharacters, '')}`
     );
     this.#checkMultipleClass(element);
-    this.#checkTextNotFound();
     this.#setCallback(element[0], this.object);
   }
 
   getMultipleClass(object) {
     this.#checkObjectNotNull(object);
     const element = document.querySelectorAll(
-      `.${object.replace(this.#mask, '')}`
+      `.${object.replace(this.#maskRemoveSpecialCharacters, '')}`
     );
     this.#checkClassNotFound(element);
     const listObject = [];
@@ -66,13 +166,12 @@ class Vel {
   }
 
   setMultipleClass(object) {
-    object = object.trim();
     this.#checkObjectNotNull(object);
+    this.#checkObjectNotFound();
     const element = document.querySelectorAll(
-      `.${object.replace(this.#mask, '')}`
+      `.${object.replace(this.#maskRemoveSpecialCharacters, '')}`
     );
     this.#checkClassNotFound(element);
-    this.#checkTextNotFound();
     element.forEach((value, index) => {
       this.#setCallback(element[index], this.object);
     });
@@ -84,7 +183,7 @@ class Vel {
     listObject.forEach((value) => {
       this.#checkObjectNotNull(value);
       let element = document.querySelectorAll(
-        `.${value.replace(this.#mask, '')}`
+        `.${value.replace(this.#maskRemoveSpecialCharacters, '')}`
       );
       this.#checkMultipleClass(element);
       element.forEach((value) => {
@@ -96,14 +195,13 @@ class Vel {
 
   setListClass(listObject) {
     this.#checkIsArray(listObject);
+    this.#checkObjectListNotFound(listObject);
     listObject.forEach((value, indexListObject) => {
-      let object = value.trim();
-      this.#checkObjectNotNull(object);
+      this.#checkObjectNotNull(value);
       let element = document.querySelectorAll(
-        `.${object.replace(this.#mask, '')}`
+        `.${value.replace(this.#maskRemoveSpecialCharacters, '')}`
       );
       this.#checkMultipleClass(element);
-      this.#checkObjectListNotFound(listObject);
       element.forEach((value, index) => {
         this.#setCallback(element[index], this.listObject[indexListObject]);
       });
@@ -144,14 +242,45 @@ class Vel {
         }
         element.src = value;
         break;
-      case Vel.typeCallback.color:
+      case Vel.typeCallback.style.color:
         element.style.color = value;
         break;
-      case Vel.typeCallback.display:
+      case Vel.typeCallback.style.display:
         element.style.display = value;
         break;
-      case Vel.typeCallback.backgroundColor:
+      case Vel.typeCallback.style.backgroundColor:
         element.style.backgroundColor = value;
+        break;
+      case Vel.typeCallback.style.zIndex:
+        element.style.zIndex = value;
+        break;
+      case Vel.typeCallback.style.backgroundImage:
+        element.style.backgroundImage = `url(${value})`;
+        break;
+      case Vel.typeCallback.style.fontSize:
+        element.style.fontSize = value;
+        break;
+      case Vel.typeCallback.style.fontWeight:
+        element.style.fontWeight = value;
+        break;
+      case Vel.typeCallback.style.height:
+        element.style.height = value;
+        break;
+      case Vel.typeCallback.style.width:
+        element.style.width = value;
+        break;
+      case Vel.typeCallback.style.visibility:
+        element.style.visibility = value;
+        break;
+      case Vel.typeCallback.style.opacity:
+        element.style.opacity = value;
+        break;
+      case Vel.typeCallback.click:
+        if (typeof value != 'function') {
+          const notFunction = this.#exceptionErrorMessage('notFunction');
+          this.#messageError(notFunction);
+        }
+        element.addEventListener('click', value);
         break;
       default:
         const callbackNotFound =
@@ -168,13 +297,13 @@ class Vel {
       this.#messageError(objectEqualNotFound);
     }
     this.listObject.forEach((value) => {
-      if (this.isEmpty(value.trim())) {
+      if (this.isEmpty(value) && typeof value != 'function') {
         const objectNotFound = this.#exceptionErrorMessage('objectNotFound');
         this.#messageError(objectNotFound);
       }
     });
     listObject.forEach((value) => {
-      if (this.isEmpty(value)) {
+      if (this.isEmpty(value) && typeof value != 'function') {
         const objectNotFound = this.#exceptionErrorMessage('objectNotFound');
         this.#messageError(objectNotFound);
       }
@@ -202,8 +331,8 @@ class Vel {
     }
   }
 
-  #checkTextNotFound() {
-    if (this.isEmpty(this.object.trim())) {
+  #checkObjectNotFound() {
+    if (this.isEmpty(this.object) && typeof this.object != 'function') {
       const objectNotFound = this.#exceptionErrorMessage('objectNotFound');
       this.#messageError(objectNotFound);
     }
@@ -237,7 +366,11 @@ class Vel {
   }
 
   #messageError(message) {
-    alert(`Error\nMessage: ${message}`);
+    if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+      alert(`Error\nMessage: ${message}`);
+    } else {
+      alert(`Erro\nMensagem: ${message}`);
+    }
     this.#exception(message);
   }
 
@@ -248,29 +381,75 @@ class Vel {
   #exceptionErrorMessage(message) {
     switch (message) {
       case 'notNull':
-        return 'The field cannot be null, it must inform a parameter.';
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'The field cannot be null, it must inform a parameter.';
+        }
+        return 'O campo não pode ser nulo, deve informar um parâmetro.';
       case 'objectNotArray':
-        return 'Enter only a list of objects.';
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'Enter only a list of objects.';
+        }
+        return 'Insira apenas uma lista de objetos.';
       case 'idNotFound':
-        return "Id not found\nMake sure your key doesn't start with numbers.";
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'Id not found.';
+        }
+        return 'Id não encontrado.';
       case 'multipleIds':
-        return 'There are one or more IDs with the same key, check the document.';
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'There are one or more IDs with the same key, check the document.';
+        }
+        return 'Existem um ou mais IDs com a mesma chave, verifique o documento.';
       case 'objectNotFound':
-        return 'You must set a object with the object property.';
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'You must set a object with the object property.';
+        }
+        return 'Você deve definir um objeto com a propriedade do objeto.';
       case 'classNotFound':
-        return 'Class not found.';
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'Class not found.';
+        }
+        return 'Classe não encontrada.';
       case 'multipleClasses':
-        return 'There are one or more Classes with the same key, check the document.';
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'There are one or more Classes with the same key, check the document.';
+        }
+        return 'Existem uma ou mais Classes com a mesma chave, verifique o documento.';
       case 'objectEqualNotFound':
-        return 'The list of objects must be the same as the parameter entered.';
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'The list of objects must be the same as the parameter entered.';
+        }
+        return 'A lista de objetos deve ser igual ao parâmetro inserido.';
       case 'callbackNotFound':
-        return 'Callback type not found or not entered.';
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'Callback type not found or not entered.';
+        }
+        return 'Tipo de retorno de chamada não encontrado ou não inserido.';
       case 'hasAttributeSrc':
-        return 'There is no src attribute on the element.';
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'There is no src attribute on the element.';
+        }
+        return 'Não há atributo src no elemento.';
       case 'resultUndefined':
-        return 'Callback result is undefined.';
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'Callback result is undefined.';
+        }
+        return 'O resultado do retorno de chamada é indefinido.';
+      case 'notFunction':
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'Object is not a function.';
+        }
+        return 'O objeto não é uma função.';
+      case 'failedAPI':
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'Failed to load date from API.';
+        }
+        return 'Falha ao carregar data da API.';
       default:
-        return 'Sorry, the error was not reported.';
+        if (this.#feedbackLanguage == Vel.feedbackLanguage.en) {
+          return 'Sorry, the error was not reported.';
+        }
+        return 'Desculpe, o erro não foi relatado.';
     }
   }
 }
